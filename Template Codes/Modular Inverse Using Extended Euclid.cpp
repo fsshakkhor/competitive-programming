93b5422304058b7b5791e5b3cdab1144cc2f6949
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2018-09-26 18:16:14 +0600

    Create Modular Inverse Using Extended Euclid.cpp

---
+++ Template Codes/Modular Inverse Using Extended Euclid.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)  freopen(x,"r",stdin)
+#define fWrite(x) freopen (x,"w",stdout)
+
+#define LL long long
+#define ULL unsigned long long
+#define ff first
+#define ss second
+#define pb push_back
+#define INF 3e18
+#define PI acos(-1.0)
+#define mk make_pair
+#define pii pair<int,int>
+
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define min4(a,b,c,d) min(a,min(b,min(c,d)))
+#define max4(a,b,c,d) max(a,max(b,max(c,d)))
+#define SQR(a) ((a)*(a))
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,a,b) for(int i=a;i>=b;i--)
+#define REP(i,b) for(int i=0;i<b;i++)
+#define MEM(a,x) memset(a,x,sizeof(a))
+#define ABS(x) ((x)<0?-(x):(x))
+
+#define scanI(a) scanf("%d",&a)
+#define scanI2(a,b) scanI(a) , scanI(b)
+#define scanI3(a,b,c) scanI(a), scanI(b), scanI(c)
+#define scanI4(a,b,c,d) scanI(a), scanI(b), scanI(c), scanI(d)
+
+#define scanL(a) scanf("%I64d",&a)
+#define scanL2(a,b) scanL(a) , scanL(b)
+#define scanL3(a,b,c) scanL(a), scanL(b), scanL(c)
+#define scanL4(a,b,c,d) scanL(a), scanL(b), scanL(c), scanL(d)
+
+#define SORT(v) sort(v.begin(),v.end())
+#define REV(v) reverse(v.begin(),v.end())
+
+#define FastRead ios_base::sync_with_stdio(0);cin.tie(nullptr);
+int egcd(int a,int b, int &x,int &y)
+{
+    if(a==0){
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1,y1;
+    int d = egcd(b%a,a,x1,y1);
+    x = y1 -(b/a)*x1;
+    y = x1;
+    return d;
+}
+
+int main()
+{
+    int x,y;
+    int g = egcd(7,10,x,y); /// we will get modular inverse of 7 with mod 10
+                            /// If x < 0 , x += 10;
+    cout << g << " " << x << " " << y << endl;
+}
