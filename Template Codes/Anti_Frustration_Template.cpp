d24a3b442da6245bf605abc06cbf58f47ee067ee
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2018-10-25 23:20:32 +0600

    Create Anti_Frustration_Template.cpp

---
+++ Template Codes/Anti_Frustration_Template.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define INF             2e16
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+
+
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+
+///For Runtime Error, Check Division By Zero, Array Access Bounds and What you pass as parameters.
+///For WA, Check Logic, Try Small TestCase, Move To The Next Problem
+///Long Long Check
+///Try Till there is a possibility of getting AC
+///And please don't start coding without analyzing complexity properly
+///If you didn't get AC for silly mistakes, don't get frustrated. You are a human.
+
+int main()
+{
+
+}
