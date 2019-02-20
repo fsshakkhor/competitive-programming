de2a8304ad0457babff60a1b36fcc36811bba5d0
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2019-02-20 17:08:40 +0600

    Create Hopcroft.cpp

---
+++ Template Codes/BPM/Hopcroft.cpp
@@ -0,0 +1,140 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define INF             2e16
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+
+
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+
+
+const int MAXN1 = 1005;
+const int MAXN2 = 10005;
+const int MAXM = 200005;
+
+int n1, n2, edges, last[MAXN1], Prev[MAXM], head[MAXM];
+int matching[MAXN2], dist[MAXN1], Q[MAXN1];
+bool used[MAXN1], vis[MAXN1];
+
+void init(int _n1, int _n2) {
+    n1 = _n1;
+    n2 = _n2;
+    edges = 0;
+    fill(last, last + n1, -1);
+}
+
+void addEdge(int u, int v) {
+    head[edges] = v;
+    Prev[edges] = last[u];
+    last[u] = edges++;
+}
+
+void bfs() {
+    fill(dist, dist + n1, -1);
+    int sizeQ = 0;
+    for (int u = 0; u < n1; ++u) {
+        if (!used[u]) {
+            Q[sizeQ++] = u;
+            dist[u] = 0;
+        }
+    }
+    for (int i = 0; i < sizeQ; i++) {
+        int u1 = Q[i];
+        for (int e = last[u1]; e >= 0; e = Prev[e]) {
+            int u2 = matching[head[e]];
+            if (u2 >= 0 && dist[u2] < 0) {
+                dist[u2] = dist[u1] + 1;
+                Q[sizeQ++] = u2;
+            }
+        }
+    }
+}
+
+bool dfs(int u1) {
+    vis[u1] = true;
+    for (int e = last[u1]; e >= 0; e = Prev[e]) {
+        int v = head[e];
+        int u2 = matching[v];
+        if (u2 < 0 || !vis[u2] && dist[u2] == dist[u1] + 1 && dfs(u2)) {
+            matching[v] = u1;
+            used[u1] = true;
+            return true;
+        }
+    }
+    return false;
+}
+
+int maxMatching() {
+    fill(used, used + n1, false);
+    fill(matching, matching + n2, -1);
+    for (int res = 0;;) {
+        bfs();
+        fill(vis, vis + n1, false);
+        int f = 0;
+        for (int u = 0; u < n1; ++u)
+            if (!used[u] && dfs(u))
+                ++f;
+        if (!f)
+            return res;
+        res += f;
+    }
+}
+
+const int N = 100005;
+bool stat[N];
+int ara[N];
+vector<int>primes,fac[1005];
+void siv()
+{
+    for(int i = 4;i < N;i += 2)stat[i] = 1;
+    for(int i = 3;i <= 1000;i += 2){
+        if(stat[i])continue;
+        for(int j = i * i;j < N;j += 2 * i)stat[j] = 1;
+    }
+    for(int i = 2;i < N;i++)if(stat[i] == 0)primes.push_back(i);
+}
+int main()
+{
+    siv();
+    int n , m;
+    scanf("%d %d",&n,&m);
+    init(n,primes.size());//Initialize First. You can use 0 number nodes;
+    for(int i = 1;i <= n;i++){
+        scanf("%d",&ara[i]);
+        for(int j = 0;j < primes.size() and primes[j] <= m;j++){
+            if(ara[i] % primes[j] == 0){
+                while(ara[i] % primes[j] == 0)ara[i]/=primes[j];
+                addEdge(i - 1,j);
+            }
+        }
+    }
+    cout << maxMatching() << "\n";
+
+}
