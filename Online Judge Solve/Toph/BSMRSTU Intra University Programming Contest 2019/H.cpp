53eb45dab84caae1384ce8cbb3f4e8d81770d7a6
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2019-12-11 00:50:44 +0600

    Create H.cpp

---
+++ Online Judge Solve/Toph/BSMRSTU Intra University Programming Contest 2019/H.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define INF             2e16
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+
+
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+
+LL solve(LL n)
+{
+    LL num = (n + 1)/2;
+    return SQR(num);
+}
+int main()
+{
+    int t,cases = 0;
+    cin >> t;
+    while(t--){
+        LL l , r;
+        cin >> l >> r;
+        cout << "Case " << ++cases << ": " << solve(r) - solve(l - 1) << "\n";
+    }
+}
