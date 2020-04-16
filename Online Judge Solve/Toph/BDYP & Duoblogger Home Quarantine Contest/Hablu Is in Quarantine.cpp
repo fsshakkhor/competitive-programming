de8c520a4dd451ffd585675724cfabdae1d9ec2e
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2020-04-17 00:02:33 +0600

    Create Hablu Is in Quarantine.cpp

---
+++ Online Judge Solve/Toph/BDYP & Duoblogger Home Quarantine Contest/Hablu Is in Quarantine.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define INF             1e9
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+#ifdef VAMP
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " = " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names+1, ',');
+        cout.write(names, comma - names) << " = " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+
+const int N = 505;
+int n;
+char str[N][N];
+int main()
+{
+    #ifdef VAMP
+        clock_t tStart = clock();
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    scanf("%d",&n);
+    REP(i,n)scanf("%s",str[i]);
+
+    int q;
+    scanf("%d",&q);
+
+    bool ok = 1;
+    vector<int>vec;
+    while(q--){
+        int num;
+        scanf("%d",&num);
+        num--;
+        int x = num/n , y = num % n;
+        if(!ok and str[x][y] == 'a')vec.push_back(num + 1);
+
+        if(str[x][y] == 'c')ok = 0;
+    }
+    if(ok){
+        cout << "NO\n";
+    }else{
+        cout << "YES\n";
+        for(int i = 0;i < vec.size();i++){
+            if(i)cout << " ";
+            cout << vec[i];
+        }
+        cout << "\n";
+    }
+    #ifdef VAMP
+        fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    #endif
+}
